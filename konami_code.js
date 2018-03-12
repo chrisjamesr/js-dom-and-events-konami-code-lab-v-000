1d4112512ca9ac623cf8ca972f326ec01671bb9c
chrisjamesr <chrisjames.r@gmail.com>
2018-03-11 20:46:00 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let codeIndex = 0
+  document.body.addEventListener('keydown', testSequence)
 
+  function testSequence(e){
+    const key = parseInt(e.which || e.detail);
+    if (key === code[codeIndex]){
+      codeIndex++;
+    } if (codeIndex === code.length) {
+      alert("Contra MF!");
+      codeIndex = 0;
+    }
+  }
 }
+
